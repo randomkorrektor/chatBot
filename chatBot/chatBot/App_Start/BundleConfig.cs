1247e7d96728b76e259534cd9f441f2805525676
RanKor <randomkorrektor@gmail.com>
2017-03-07 23:14:46 +0700

    text error frontEnd

--- chatBot/chatBot/App_Start/BundleConfig.cs
+++ chatBot/chatBot/App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace chatBot
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/VALERA").Include(
+                                   "~/Scripts/VALERA.js",
+                                   "~/Scripts/VALERAUMENSHI"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
